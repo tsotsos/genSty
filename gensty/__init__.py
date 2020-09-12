017f2bd6e11b5e0474e1be374d7cfdf6e3269074
Georgios Tsotsos <tsotsos@gmail.com>
2020-09-12 14:06:58 +0300

    Imports submodules.
    Signed-off-by: Georgios Tsotsos <tsotsos@gmail.com>

--- gensty/__init__.py
+++ gensty/__init__.py
@@ -1,3 +1,10 @@
+import gensty.helpers
+import gensty.font
 from .gensty import __version__
 from .gensty import __email__
 from .gensty import __author__
+
+SUPPORTED_FONTS = ['ttf', 'otf']
+__header_template = 'resources/header.sty'
+__commands_template = 'resources/defcommands.sty'
+
