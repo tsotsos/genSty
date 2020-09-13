23bfac901e2a8b8b79159701654b91cffb4f3fa9
Georgios Tsotsos <tsotsos@gmail.com>
2020-09-13 17:54:18 +0300

    Config.py holds constants.
    Signed-off-by: Georgios Tsotsos <tsotsos@gmail.com>

---
+++ gensty/config.py
@@ -0,0 +1,9 @@
+""" Config defines some important globals."""
+SUPPORTED_FONTS     = ['ttf', 'otf']
+LATEX_REQUIREMENTS  = ['fontspec']
+FONTDIR             = "fonts"
+HEADER_TEMPLATE     = 'resources/header.sty'
+COMMANDS_TEMPLATE   = 'resources/defcommands.sty'
+__author__ = 'Georgios Tsotsos'
+__email__ = 'tsotsos@gmail.com'
+__version__ = '0.2.7'
