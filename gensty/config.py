f9d689ae1cfb8b944bebd576707bdc5f8f1c2bd3
Georgios Tsotsos <tsotsos@gmail.com>
2020-09-15 00:41:40 +0300

    Adds a file description and a new version.
    Signed-off-by: Georgios Tsotsos <tsotsos@gmail.com>

--- gensty/config.py
+++ gensty/config.py
@@ -1,9 +1,9 @@
-""" Config defines some important globals."""
+""" Config file defines some important globals."""
 SUPPORTED_FONTS     = ['ttf', 'otf']
 LATEX_REQUIREMENTS  = ['fontspec']
 FONTDIR             = "fonts"
 HEADER_TEMPLATE     = 'resources/header.sty'
 COMMANDS_TEMPLATE   = 'resources/defcommands.sty'
-__author__ = 'Georgios Tsotsos'
-__email__ = 'tsotsos@gmail.com'
-__version__ = '0.2.7'
+__author__          = 'Georgios Tsotsos'
+__email__           = 'tsotsos@gmail.com'
+__version__         = '0.2.9'
